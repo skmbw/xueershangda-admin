0b9b46d8de939b9d8aee92a1c5babd4241bda892
skmbw <tongku2008@126.com>
2019-08-20 21:10:57 +0800

    新增两个icon图标

--- src/style-icons.ts
+++ src/style-icons.ts
@@ -1,11 +1,15 @@
 // Custom icon static resources
 
 import {
-  InfoOutline,
   BulbOutline,
-  ProfileOutline,
   ExceptionOutline,
+  FileTextOutline,
+  InfoOutline,
   LinkOutline,
+  ProfileOutline,
+  VideoCameraOutline,
 } from '@ant-design/icons-angular/icons';
 
-export const ICONS = [InfoOutline, BulbOutline, ProfileOutline, ExceptionOutline, LinkOutline];
+export const ICONS = [InfoOutline, BulbOutline, ProfileOutline, ExceptionOutline, LinkOutline,
+  VideoCameraOutline,
+  FileTextOutline];
