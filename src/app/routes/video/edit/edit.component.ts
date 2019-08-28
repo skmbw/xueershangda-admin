67e761314a6948d30872f0bcc5bf0dcb5a766a5b
skmbw <tongku2008@126.com>
2019-08-28 11:18:21 +0800

    尝试引入上传组件

--- src/app/routes/video/edit/edit.component.ts
+++ src/app/routes/video/edit/edit.component.ts
@@ -6,12 +6,16 @@ import { JsUtils } from '@shared/utils/js-utils';
 import { Consts } from '@shared/utils/consts';
 import { VideoService } from '@shared/service/video.service';
 import { com } from '@shared';
+import * as $ from 'jquery';
 import Video = com.xueershangda.tianxun.video.model.Video;
 import VideoReply = com.xueershangda.tianxun.video.model.VideoReply;
+
+declare var plupload: any;
 
 @Component({
   selector: 'app-video-edit',
   templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.css']
 })
 export class VideoEditComponent implements OnInit {
   record: any;
@@ -161,6 +165,63 @@ export class VideoEditComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    // Initialize the widget when the DOM is ready
+    const uploader = $("#uploader").pluploadQueue({
+      // General settings
+      runtimes: 'html5,flash,silverlight,html4',
+      url: "../pluploadUpload",
+
+      // Maximum file size
+      max_file_size: '10000mb',
+
+      chunk_size: '1mb',
+
+      // Resize images on clientside if we can
+      resize: {
+        width: 200,
+        height: 200,
+        quality: 90,
+        crop: true // crop to exact dimensions
+      },
+
+      // Specify what files to browse for
+      filters: [
+        {title: "Image files", extensions: "jpg,gif,png,jpeg"},
+        {title: "Vedio files", extensions: "mp4,mkv"},
+        {title: "Zip files", extensions: "zip,avi"}
+      ],
+
+      // Rename files by clicking on their titles
+      rename: true,
+
+      // Sort files
+      sortable: true,
+
+      // Enable ability to drag'n'drop files onto the widget (currently only HTML5 supports that)
+      dragdrop: true,
+
+      // Views to activate
+      views: {
+        list: true,
+        thumbs: true, // Show thumbs
+        active: 'thumbs'
+      },
+
+      // Flash settings
+      // flash_swf_url: 'js/Moxie.swf',
+
+      // Silverlight settings
+      // silverlight_xap_url: 'js/Moxie.xap'
+    });
+
+    // $("#toStop").on('click', function () {
+    //   uploader.stop();
+    // });
+    //
+    // $("#toStart").on('click', function () {
+    //   uploader.start();
+    // });
+
     // alert(this.record.id); // this.undefined就是穿过来的参数，但是table.js._btnClick中的bug，没有设置或取到参数名导致
     // if (this.record.id > 0)
     //   this.http.get(Consts.URL + `/video/detail/${this.record.id}`).subscribe(res => (this.i = res));
