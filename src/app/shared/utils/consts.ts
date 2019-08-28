78b84204943860cdaa5eb6352c8a28613c131e2b
skmbw <tongku2008@126.com>
2019-08-28 14:31:16 +0800

    以后const这个变量就不需要改动了

--- src/app/shared/utils/consts.ts
+++ src/app/shared/utils/consts.ts
@@ -1,9 +1,11 @@
 import { HttpHeaders } from '@angular/common/http';
+import { IP } from '@env/environment';
 
 export class Consts {
-  static URL = 'http://localhost:8775/';
-  static IMAGE_HOST = 'http://locahost:8300/';
-  static IMAGE_URL = 'localhost:8300/';
+  static IP = IP;
+  static URL = `http://${Consts.IP}:8775/`;
+  static IMAGE_HOST = `http://${Consts.IP}:8300/`;
+  static IMAGE_URL = `${Consts.IP}:8300/`;
   static JSON = {
     headers: new HttpHeaders({'Content-Type': 'application/json;charset=utf-8'})
   };
